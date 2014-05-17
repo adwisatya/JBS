e0bb071389bc95dd76218610180f79f14c6ad21f
adwisatya <a.dwisaty4@yahoo.com>
2014-05-17 22:24:19 +0700

    Java BackDoor Scanner

--- src/backdoorscanner/BackdoorScanner.java
+++ src/backdoorscanner/BackdoorScanner.java
@@ -18,29 +18,50 @@ import static pathManager.pathManager.*;
 public class BackdoorScanner {
     public static void main(String[] args) {
         // TODO code application logic here
-        String filePath    =   "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Backup\\2012\\Desember\\Index.php";
-        checkFile(filePath);
+        String [] filePath    =   
+        {
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Backup\\2012\\Desember\\Index.php",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Backup\\2013\\Desember\\Index.old.php",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Backup\\2013\\Januari\\Connect.php",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Backup\\2013\\Januari\\Index.php",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Backup\\2013\\Januari\\Readme.txt",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Backup\\2013\\Januari\\Trial.php",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Backup\\2013\\Juni\\Config.php",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Backup\\2014",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Config",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Css,Bootstrap.css",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Files",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Images",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Images\\Header.jpg",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Images\\Footer.jpg",
+            "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Images\\Background.jpg",
+        };
+        for(int i=0;i<filePath.length;i++){
+            checkFile(filePath[i]);
+        }
     }
     public static void checkFile(String filePath){
         String [] pattern  =   {"system(","passthru(","exec(","shell_exec("};
         String pathFull = getPath(filePath);
         String fileName = getFile(filePath);  
-        String tempString;
-        boolean isBackdoor  =    false;
-        try{
-            FileInputStream fstream = new FileInputStream(filePath);
-            BufferedReader    tempLine    =    new BufferedReader(new InputStreamReader(fstream));
-            while(((tempString = tempLine.readLine()) != null) && !isBackdoor){
-                for(int i=0;i<pattern.length;i++){
-                       isBackdoor  =   isBackdoor || (kmpMatch(tempString,pattern[i]) != -1);
-                }
-            }     
-        }catch(Exception e){
-            // do nothing
-        }    
-        if(isBackdoor){
-            System.out.println("Directory: " + pathFull);
-            System.out.println(fileName + " is backdoor");
+        if(fileName.contains(".php")){
+            String tempString;
+            boolean isBackdoor  =    false;
+            try{
+                FileInputStream fstream = new FileInputStream(filePath);
+                BufferedReader    tempLine    =    new BufferedReader(new InputStreamReader(fstream));
+                while(((tempString = tempLine.readLine()) != null) && !isBackdoor){
+                    for(int i=0;i<pattern.length;i++){
+                           isBackdoor  =   isBackdoor || (kmpMatch(tempString,pattern[i]) != -1);
+                    }
+                }     
+            }catch(Exception e){
+                // do nothing
+            }    
+            if(isBackdoor){
+                System.out.println("Directory: " + pathFull);
+                System.out.println(fileName + " is backdoor");
+            }
         }
     }
 }
