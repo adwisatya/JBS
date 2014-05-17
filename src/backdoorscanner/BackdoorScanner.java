3d145e2a2170f6fcc695341c48a8770aa9efbb34
adwisatya <a.dwisaty4@yahoo.com>
2014-05-17 22:02:02 +0700

    Java BackDoor Scanner

--- src/backdoorscanner/BackdoorScanner.java
+++ src/backdoorscanner/BackdoorScanner.java
@@ -18,8 +18,11 @@ import static pathManager.pathManager.*;
 public class BackdoorScanner {
     public static void main(String[] args) {
         // TODO code application logic here
-        String pattern  =   "system(";
-        String filePath    =   "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\backdoorscanner\\backdoor.php";
+        String filePath    =   "C:\\Users\\adwisatya\\Documents\\NetBeansProjects\\BackdoorScanner\\src\\root\\Backup\\2012\\Desember\\Index.php";
+        checkFile(filePath);
+    }
+    public static void checkFile(String filePath){
+        String [] pattern  =   {"system(","passthru(","exec(","shell_exec("};
         String pathFull = getPath(filePath);
         String fileName = getFile(filePath);  
         String tempString;
@@ -27,19 +30,17 @@ public class BackdoorScanner {
         try{
             FileInputStream fstream = new FileInputStream(filePath);
             BufferedReader    tempLine    =    new BufferedReader(new InputStreamReader(fstream));
-                  
             while(((tempString = tempLine.readLine()) != null) && !isBackdoor){
-                if(kmpMatch(tempString,pattern) != -1){
-                    isBackdoor  =   true;
+                for(int i=0;i<pattern.length;i++){
+                       isBackdoor  =   isBackdoor || (kmpMatch(tempString,pattern[i]) != -1);
                 }
             }     
-            if(isBackdoor){
-                System.out.println("Directory: " + pathFull);
-                System.out.println(fileName + " is backdoor");
-            }
         }catch(Exception e){
-            System.out.println("Terdapat kesalahan pembacaan");
+            // do nothing
+        }    
+        if(isBackdoor){
+            System.out.println("Directory: " + pathFull);
+            System.out.println(fileName + " is backdoor");
         }
     }
-
 }
